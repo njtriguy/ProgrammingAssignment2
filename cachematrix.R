02b28c9a1aa39e1ecd9c3166118a8e817990911b
njtriguy <darrenfreeman@gmail.com>
2014-12-16 09:39:29 -0500

    Added Comments to document what the functions do.

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,46 @@
+## Darren Freeman
+## Assignment #2 
+## R programming December 2014
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
+## This function takes a matrix, calculates the invers and stores 
+## it in the global environment.  The purpose of this is the reduce
+## the overhead and processing time which is required to calculate 
+## the inverse and speeds the calculation of downstream calculations
 
 makeCacheMatrix <- function(x = matrix()) {
-    matrixCache <- NULL
+    matrixcache <- NULL
     set <- function(y) {
         x <<- y
         matrixCache <<- NA
     }
     get <- function() x
-    setMatrix <- function(solve) matrixCache <<- solve
-    getMatrix <- function() matrixCache
-    list(set=set,get=get,setMatrix=setMatrix,getMatrix=getMatrix)
+    setmatrix <- function(solve) matrixcache <<- solve
+    getmatrix <- function() matrixcache
+    list(set=set,get=get,
+         setmatrix=setmatrix,
+         getmatrix=getmatrix)
 }
 
 
 ## Write a short comment describing this function
+## This function checks to see if the cached matrix inverse
+## is already stored in the global environment.  If it already
+## exists the cached inverse is returned, if it is not cached 
+## the inverse is calculated and stored in the global environment
+## and then returns the value to the function caller.x
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        matrixCache <- x$getMatrix()
-        if(!is.null(matrixCache)&&is.matrix(matrixCache)) {
+        matrixcache <- x$getmatrix()
+        if(!is.null(matrixcache)) {
             message("getting cached matrix")
-            return(matrixCache)
+            return(matrixcache)
         }
         data<-x$get()
-        matrixCache<-solve(data,...)
-        x$setMatrix(matrixCache)
-        matrixCache
+        matrixcache<-solve(data,...)
+        x$setmatrix(matrixcache)
+        matrixcache
 }
