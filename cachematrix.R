a8d11789690e1b692cf528ff4fcff7276b969239
njtriguy <darrenfreeman@gmail.com>
2014-12-16 08:41:04 -0500

    First Commit Save of CacheMatrix Functions

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    matrixCache <- NULL
+    set <- function(y) {
+        x <<- y
+        matrixCache <<- NA
+    }
+    get <- function() x
+    setMatrix <- function(solve) matrixCache <<- solve
+    getMatrix <- function() matrixCache
+    list(set=set,get=get,setMatrix=setMatrix,getMatrix=getMatrix)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        matrixCache <- x$getMatrix()
+        if(!is.null(matrixCache)&&is.matrix(matrixCache)) {
+            message("getting cached matrix")
+            return(matrixCache)
+        }
+        data<-x$get()
+        matrixCache<-solve(data,...)
+        x$setMatrix(matrixCache)
+        matrixCache
 }
